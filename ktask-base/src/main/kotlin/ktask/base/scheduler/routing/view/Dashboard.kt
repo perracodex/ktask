f3971278271357b91ed83310dc9a3382bde9ed39
Fernando Sanchez <perracolabs@gmail.com>
2024-06-18 20:27:36 +0200

    Added group selection

--- ktask-base/src/main/kotlin/ktask/base/scheduler/routing/view/Dashboard.kt
+++ ktask-base/src/main/kotlin/ktask/base/scheduler/routing/view/Dashboard.kt
@@ -8,8 +8,10 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
+import ktask.base.persistence.utils.toUUIDOrNull
 import ktask.base.scheduler.entity.TaskScheduleEntity
 import ktask.base.scheduler.service.core.SchedulerService
+import java.util.*
 
 /**
  * The scheduler dashboard route.
@@ -18,7 +20,8 @@ fun Route.schedulerDashboardRoute() {
 
     // The scheduler dashboard route.
     get("dashboard") {
-        val tasks: List<TaskScheduleEntity> = SchedulerService.tasks.all()
+        val groupId: UUID? = call.parameters["group"]?.let { it.toUUIDOrNull() }
+        val tasks: List<TaskScheduleEntity> = SchedulerService.tasks.all(groupId = groupId)
         val content = ThymeleafContent(template = "scheduler/dashboard", model = mapOf("data" to tasks))
         call.respond(message = content)
     }
