3d22e3fff269bf34852cf0f422276e1dd07c785d
Fernando Sanchez <perracolabs@gmail.com>
2024-09-17 23:08:45 +0200

    Simplified routes file names

--- ktask-base/src/main/kotlin/ktask/base/scheduler/api/SchedulerRoutes.kt
+++ ktask-base/src/main/kotlin/ktask/base/scheduler/api/SchedulerRoutes.kt
@@ -8,10 +8,10 @@ import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import ktask.base.scheduler.api.scheduler.audit.schedulerAllAuditRoute
 import ktask.base.scheduler.api.scheduler.audit.schedulerAuditByTaskRoute
-import ktask.base.scheduler.api.scheduler.pauseSchedulerRoute
-import ktask.base.scheduler.api.scheduler.restartSchedulerRoute
-import ktask.base.scheduler.api.scheduler.resumeSchedulerRoute
-import ktask.base.scheduler.api.scheduler.schedulerStateRoute
+import ktask.base.scheduler.api.scheduler.operate.pauseSchedulerRoute
+import ktask.base.scheduler.api.scheduler.operate.restartSchedulerRoute
+import ktask.base.scheduler.api.scheduler.operate.resumeSchedulerRoute
+import ktask.base.scheduler.api.scheduler.operate.schedulerStateRoute
 import ktask.base.scheduler.api.tasks.delete.deleteAllSchedulerTasksRoute
 import ktask.base.scheduler.api.tasks.delete.deleteSchedulerTaskRoute
 import ktask.base.scheduler.api.tasks.get.getSchedulerTaskGroupsRoute
--- ktask-base/src/main/kotlin/ktask/base/scheduler/api/scheduler/operate/Resume.kt
+++ ktask-base/src/main/kotlin/ktask/base/scheduler/api/scheduler/operate/Resume.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2024-Present Perracodex. Use of this source code is governed by an MIT license.
  */
 
-package ktask.base.scheduler.api.scheduler
+package ktask.base.scheduler.api.scheduler.operate
 
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -12,15 +12,15 @@ import ktask.base.scheduler.model.task.TaskStateChange
 import ktask.base.scheduler.service.core.SchedulerService
 
 /**
- * Pauses all the scheduler tasks.
+ * Resume all the scheduler tasks.
  */
-internal fun Route.pauseSchedulerRoute() {
+internal fun Route.resumeSchedulerRoute() {
     /**
-     * Pauses all the scheduler tasks.
+     * Resume all the scheduler tasks.
      * @OpenAPITag Scheduler - Maintenance
      */
-    post("scheduler/pause") {
-        val state: TaskStateChange = SchedulerService.pause()
+    post("scheduler/resume") {
+        val state: TaskStateChange = SchedulerService.resume()
         call.respond(status = HttpStatusCode.OK, message = state)
     }
 }
