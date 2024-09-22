a1eb99619aeb4b2ba7ab68d2c40cec612e6bb006
Fernando Sanchez <perracolabs@gmail.com>
2024-09-22 20:45:34 +0200

    Updated dependencies. kotlinx-serialization:1.7.3

--- ktask-notification/build.gradle.kts
+++ ktask-notification/build.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
     implementation(project(":ktask-base"))
 
     implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization)
 
-    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.auth)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
